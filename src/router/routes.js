5d04e1ddb19dc5626707795f4cf91556bc2bc1be
jiajing <buer_jing@163.com>
2017-09-19 18:02:44 +0800

    1

---
+++ src/router/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route} from 'react-router';
+import Index from '../components/Main.js';
+
+const routes = (
+	<Route>
+		<Route path="/" component={Index}>
+
+		</Route>
+	</Route>
+
+)
